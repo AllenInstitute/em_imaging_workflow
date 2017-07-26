ff34aa8875deb454bb3dd37f7928f19125f843a6
Nathan Sjoquist <nathans@alleninstitute.org>
2017-07-26 15:58:01 -0700

    updating execution_runner

--- execution_runner.py
+++ execution_runner.py
@@ -12,36 +12,37 @@ SUCCESS_EXIT_CODE = 0
 ERROR_EXIT_CODE = 1
 
 @app.task
-def run_celery_task(full_executable, task_id, logfile):
-
+def publish_message(body, task_id):
 	connection = pika.BlockingConnection(pika.ConnectionParameters(host=MESSAGE_QUEUE_HOST))
 	channel = connection.channel()
+	channel.queue_declare(queue='tasks')
+	channel.basic_publish(exchange='', routing_key='tasks', body= body + ',' + str(task_id))
+	connection.close()
 
+@app.task
+def run_celery_task(full_executable, task_id, logfile):
 	exit_code = SUCCESS_EXIT_CODE
 
-	channel.queue_declare(queue='tasks')
-	channel.basic_publish(exchange='', routing_key='tasks', body='RUNNING,' + str(task_id))
-	
 	try:
+		publish_message('RUNNING', task_id)
+
 		exit_code = os.system(full_executable)
 
 		if exit_code == SUCCESS_EXIT_CODE:
-			channel.basic_publish(exchange='', routing_key='tasks', body='FINISHED_EXECUTION,' + str(task_id))
+			publish_message('FINISHED_EXECUTION', task_id)
 
 			with open(logfile, "a") as log:
 				log.write("SUCCESS - execution finished successfully for task " + str(task_id))
 
 		else:
-			channel.basic_publish(exchange='', routing_key='tasks', body='FAILED_EXECUTION,' + str(task_id))
+			publish_message('FAILED_EXECUTION', task_id)
 
 			with open(logfile, "a") as log:
 				log.write("FAILURE - execution failed for task " + str(task_id))
 
 	except Exception as e:
 		exit_code = ERROR_EXIT_CODE
-		channel.basic_publish(exchange='', routing_key='tasks', body='FAILED_EXECUTION,' + str(task_id))
+		publish_message('FAILED_EXECUTION', task_id)
 
 	return exit_code
-
-	connection.close()
 
