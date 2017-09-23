14bab4a9cc97d7a653e1309c1d1b361f1acb4b31
Tim Fliss <timf@alleninstitute.org>
2017-09-22 17:16:19 -0700

    at_em models in admin.py

--- development/admin.py
+++ development/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from development.models import *
 
 # Register your models here.
+admin.site.register(ATMontageSet)
+admin.site.register(Camera)
+admin.site.register(Chuck)
+admin.site.register(EMMontageSet)
+admin.site.register(Load)
+admin.site.register(Microscope)
+admin.site.register(MicroscopeType)
+admin.site.register(MontageSet)
+admin.site.register(RegistrationSeries)
+admin.site.register(ReferenceSet)
+admin.site.register(RenderedVolumn)
+admin.site.register(SampleHolder)
+admin.site.register(Section)
+admin.site.register(Stain)
+admin.site.register(Specimen)
+admin.site.register(Study)
+admin.site.register(TileImageSet)
