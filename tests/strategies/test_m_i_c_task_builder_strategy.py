08cebd69f4a16b52f07d053f1680f57c959a7d31
Tim Fliss <timf@alleninstitute.org[H>
2017-09-27 23:34:12 +0000

    stub strategy tests

---
+++ tests/strategies/test_m_i_c_task_builder_strategy.py
@@ -0,0 +1,15 @@
+import django
+django.setup()
+from django.test import TestCase
+from development.strategies import CreateTilePairsStrategy
+
+class TestMICTaskBuilderStrategy(TestCase):
+    def test_get_input_data(self):
+        enqueued_object = None
+        task = None
+        storage_directory = '/example/storage/directory'
+        strategy = CreateTilePairsStrategy()
+        input = strategy.get_input(enqueued_object,
+                                   storage_directory,
+                                   task)
+        assert input is not None
