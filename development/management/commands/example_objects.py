4c62702168e98b100c18e7500983fd797d9846f3
Tim Fliss <timf@alleninstitute.org>
2017-10-16 17:11:40 -0700

    example object fix and test

--- development/management/commands/example_objects.py
+++ development/management/commands/example_objects.py
@@ -46,114 +46,139 @@ _log = logging.getLogger(
 class Command(BaseCommand):
     help = 'Generate Example Objects (Microscope, Camera, etc.)'
 
+    @classmethod
+    def example_reference_set(cls):
+        camera, _ = \
+            Camera.objects.update_or_create(
+                uid="DEADBEEF")
+
+        microscope_type, _ = \
+            MicroscopeType.objects.update_or_create(
+                name="DEADBEEF")
+
+        microscope, _ = \
+            Microscope.objects.update_or_create(
+                uid="DEADBEEF",
+                defaults={
+                    'microscope_type': microscope_type
+                })
+
+        reference_set, _ = \
+            ReferenceSet.objects.update_or_create(
+                uid="DEADBEEF",
+                defaults={
+                    'storage_directory': '/example_data',
+                    'workflow_state': 'Lorem Ipsum',
+                    'camera': camera,
+                    'microscope': microscope,
+                    'project_path': '/example_data'
+                })
+
+        return reference_set
+
+    @classmethod
+    def example_em_montage_set(cls):
+        _log.info('creating study')
+        study, _ = Study.objects.update_or_create(
+            name='DEADBEEF',
+            defaults={
+                'storage_directory': '/study/directory'
+            })
+
+        _log.info('creating specimen')
+        specimen, _ = Specimen.objects.update_or_create(
+            uid='DEADBEEF',
+            defaults={
+                'render_project': 'PROJECT Lorem Impsum',
+                'render_owner': 'Lorem Imsum',
+                'study': study
+            })
+
+        _log.info('creating section')
+        section, _ = Section.objects.update_or_create(
+            id='12345',
+            defaults={
+                'z_index': 98765,
+                'metadata': None,
+                'specimen': specimen 
+                # TODO: many-to-many fields
+                # 'chucks': None,    # TODO ???
+                # 'sample_holders'
+            })
+
+        _log.info('creating load')
+        load, _ = Load.objects.update_or_create(
+            uid='DEADBEEF'
+        )
+
+
+        _log.info('creating sample holder')
+        sample_holder, _ = SampleHolder.objects.update_or_create(
+            uid='DEADBEEF',
+            defaults={
+                'imaged_sections_count': 0,
+                'load': load
+            })
+
+        _log.info('creating em montage set')
+        example_reference_set = cls.example_reference_set()
+        em_montage_set, _ = EMMontageSet.objects.update_or_create(
+            uid="DEADBEEF",
+            defaults={
+                'acquisition_date': None,
+                'mipmap_directory': '/mip/map/directory',
+                'section': section,
+                'sample_holder': sample_holder,
+                'reference_set': example_reference_set,
+                'reference_set_uid': example_reference_set.uid
+         
+            })
+
+    @classmethod
+    def fix_nathans_queue(cls):
+        # fix strategy and enqueued object classes for existing workflow
+        apply_montage_set_queue, _ = \
+            JobQueue.objects.update_or_create(
+                name='apply_to_previously_uploaded_montage_sets_queue',
+                defaults={
+                    'description': 'N/A',
+                    'job_strategy_class': 'development.strategies.' + \
+                        'apply_to_previously_uploaded_' + \
+                        'montage_sets_strategy.' + \
+                        'ApplyToPreviouslyUploadedMontageSetsStrategy',
+                    'enqueued_object_class': 'development.models.' + \
+                        'ReferenceSet'
+                })
+        generate_lens_correction_transform_queue, _ = \
+            JobQueue.objects.update_or_create(
+                name='generate_lens_correction_transform_queue',
+                defaults={
+                    'description': 'N/A',
+                    'job_strategy_class': 'development.strategies.' + \
+                        'generate_lens_correction_transform_strategy.' + \
+                        'GenerateLensCorrectionTransformStrategy',
+                    'enqueued_object_class': 'development.models.' + \
+                        'ReferenceSet'
+                })
+
     def add_arguments(self, parser):
         # parser.add_argument('file')
         pass
 
     def handle(self, *args, **options):
         # file_path = options['file']
-
         try:
-            camera, _ = \
-                Camera.objects.update_or_create(
-                    uid="DEADBEEF")
-
-            microscope_type, _ = \
-                MicroscopeType.objects.update_or_create(
-                    name="DEADBEEF")
-
-            microscope, _ = \
-                Microscope.objects.update_or_create(
-                    uid="DEADBEEF",
-                    defaults={
-                        'microscope_type': microscope_type
-                    })
-
-            reference_set, _ = \
-                ReferenceSet.objects.update_or_create(
-                    uid="DEADBEEF",
-                    defaults={
-                        'storage_directory': '/example_data',
-                        'workflow_state': 'Lorem Ipsum',
-                        'camera': camera,
-                        'microscope': microscope,
-                        'project_path': '/example_data'
-                    })
-
-
-            # fix strategy and enqueued object classes for existing workflow
-            apply_montage_set_queue, _ = \
-                JobQueue.objects.update_or_create(
-                    name='apply_to_previously_uploaded_montage_sets_queue',
-                    defaults={
-                        'description': 'N/A',
-                        'job_strategy_class': 'development.strategies.apply_to_previously_uploaded_montage_sets_strategy.ApplyToPreviouslyUploadedMontageSetsStrategy',
-                        'enqueued_object_class': 'development.models.ReferenceSet'
-                    })
-            generate_lens_correction_transform_queue, _ = \
-                JobQueue.objects.update_or_create(
-                    name='generate_lens_correction_transform_queue',
-                    defaults={
-                        'description': 'N/A',
-                        'job_strategy_class': 'development.strategies.generate_lens_correction_transform_strategy.GenerateLensCorrectionTransformStrategy',
-                        'enqueued_object_class': 'development.models.ReferenceSet'
-                    })
-
+            self.__class__.example_reference_set()
         except Exception as e:
             _log.error('Something went wrong: ' + str(e))
 
         try:
-            _log.info('creating study')
-            study, _ = Study.objects.update_or_create(
-                name='DEADBEEF',
-                defaults={
-                    'storage_directory': '/study/directory'
-                })
-
-            _log.info('creating specimen')
-            specimen, _ = Specimen.objects.update_or_create(
-                uid='DEADBEEF',
-                defaults={
-                    'render_project': 'PROJECT Lorem Impsum',
-                    'render_owner': 'Lorem Imsum',
-                    'study': study
-                })
-
-            _log.info('creating section')
-            section, _ = Section.objects.update_or_create(
-                id='12345',
-                defaults={
-                    'z_index': 98765,
-                    'metadata': None,
-                    'specimen': specimen 
-                    # TODO: many-to-many fields
-                    # 'chucks': None,    # TODO ???
-                    # 'sample_holders'
-                })
-
-            _log.info('creating load')
-            load, _ = Load.objects.update_or_create(
-                uid='DEADBEEF'
-            )
-
-
-            _log.info('creating sample holder')
-            sample_holder, _ = SampleHolder.objects.update_or_create(
-                uid='DEADBEEF',
-                defaults={
-                    'imaged_sections_count': 0,
-                    'load': load
-                })
-
-            _log.info('creating em montage set')
-            em_montage_set, _ = EMMontageSet.objects.update_or_create(
-                uid="DEADBEEF",
-                defaults={
-                    'acquisition_date': None,
-                    'mipmap_directory': '/mip/map/directory',
-                    'section': section,
-                    'sample_holder': sample_holder
-                })
+            self.__class__.example_em_montage_set()
         except Exception as e:
-            _log.error('Something went wrong 2: ' + str(e))
-       
+            _log.error('Something went wrong: ' + str(e))
+
+        try:
+            self.__class__.fix_nathans_queue()
+        except Exception as e:
+            _log.error('Something went wrong: ' + str(e))
+
