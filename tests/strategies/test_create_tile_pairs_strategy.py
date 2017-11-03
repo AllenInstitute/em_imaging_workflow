cc0111714888d7d1330775d18a53fa2ad66bc4d1
Tim Fliss <timf@alleninstitute.org>
2017-11-03 12:28:19 -0700

    test fix

--- tests/strategies/test_create_tile_pairs_strategy.py
+++ tests/strategies/test_create_tile_pairs_strategy.py
@@ -1,17 +1,19 @@
 import django
 django.setup()
 from django.test import TestCase
+from mock import Mock, patch
 from development.strategies.create_tile_pairs_strategy \
     import CreateTilePairsStrategy
 
 class TestCreateTilePairsStrategy(TestCase):
     def test_get_input_data(self):
         enqueued_object = None
-        task = None
+        task = Mock()
         storage_directory = '/example/storage/directory'
         strategy = CreateTilePairsStrategy()
-        input = strategy.get_input(enqueued_object,
-                                   storage_directory,
-                                   task)
+        with patch('os.makedirs'):
+            input = strategy.get_input(enqueued_object,
+                                       storage_directory,
+                                       task)
         assert input is not None
 
