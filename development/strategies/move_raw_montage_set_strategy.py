ff5a2f345cdd1628cfc1d796af32413222252e5d
Tim Fliss <timf@alleninstitute.org>
2017-11-08 18:11:58 -0800

    em-131db config fix and move strategy

--- development/strategies/move_raw_montage_set_strategy.py
+++ development/strategies/move_raw_montage_set_strategy.py
@@ -19,10 +19,9 @@ class MoveRawMontageSetStrategy(execution_strategy.ExecutionStrategy):
   #called after the execution finishes
   #process and save results to the database
   def on_finishing(self, enqueued_object, results, task):
-
-    self.check_key(results, 'output_json')
-
-    self.set_well_known_file(results['output_json'], enqueued_object, 'description', task)
+    # self.check_key(results, 'output_json')
+    # self.set_well_known_file(results['output_json'], enqueued_object, 'description', task)
+    pass
 
   #override if needed
   #set the storage directory for an enqueued object
