aafd53a0e894437260dc39ec90fdfd1679710e87
Tim Fliss <timf@alleninstitute.org>
2017-11-10 16:39:57 -0800

    start reference ingest worker on staging

--- staging_entry.sh
+++ staging_entry.sh
@@ -14,6 +14,7 @@ python manage.py import_workflows tests/workflows/dev.yml
 python -m celery flower --backend=rpc:// --broker=amqp://blue_sky_user:blue_sky_user@message_queue:5672 -n flower@at_em_image_processing &
 # python -m celery -A execution_runner worker --loglevel=debug --concurrency=2 -Q at_em_imaging_workflow -n at_em_worker@at_em_image_processing &
 python manage.py ingest_worker &
+python manage.py ingest_reference_set &
 export DJANGO_SETTINGS_MODULE=at_em_imaging_workflow.staging_server_worker_settings
 python manage.py run_execution_worker&
 export DJANGO_SETTINGS_MODULE=at_em_imaging_workflow.staging_settings
