5e9bd760ad0a2c912e80f4e78fb0dd40f0d9b8a1
Tim Fliss <timf@alleninstitute.org>
2017-12-20 10:51:49 -0800

    get workflow yaml from settings in entry.sh

--- entry.sh
+++ entry.sh
@@ -8,13 +8,16 @@ sleep 15
 DEBUG_LOG=logs/migrate.log python -m manage migrate  --noinput
 sleep 15
 
+export WORKFLOW_CONFIG_YAML=$(python -c "import ${DJANGO_SETTINGS_MODULE} as settings; print(settings.WORKFLOW_CONFIG_YAML)")
+
 export DEBUG_LOG=logs/create_superuser.log
 echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('blue_sky_user', 'admin@example.com', 'blue_sky_user')" | python manage.py shell
 
 export DEBUG_LOG=logs/superuser_pass.log
 printf "blue_sky_user\nt@a.org\nblue_sky_user\n" | python manage.py createsuperuser
 
-DEBUG_LOG=logs/import_workflows.log python -m manage import_workflows tests/workflows/dev.yml
+echo "reading workflows from workflow config yaml: " ${WORKFLOW_CONFIG_YAML}
+DEBUG_LOG=logs/import_workflows.log python -m manage import_workflows ${WORKFLOW_CONFIG_YAML}
 
 # Monitoring
 python -m celery flower --backend=rpc:// --broker=amqp://blue_sky_user:blue_sky_user@${MESSAGE_QUEUE_HOST}:5672 -n flower@${APP_NAME} &
