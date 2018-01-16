c2bb4d04033585d084f6d712ce6611301d20c0a4
Tim Fliss <timf@alleninstitute.org>
2018-01-16 10:36:19 -0800

    CONNP-280; fix test

--- tests/strategies/test_move_raw_montage_set_strategy.py
+++ tests/strategies/test_move_raw_montage_set_strategy.py
@@ -1,14 +1,15 @@
 import pytest
+from mock import Mock
 from development.strategies.move_raw_montage_set_strategy \
     import MoveRawMontageSetStrategy
 
 
 def test_get_input_data():
-    enqueued_object = None
-    task = None
+    enqueued_object = Mock()
+    task = Mock()
     storage_directory = '/example/storage/directory'
     strategy = MoveRawMontageSetStrategy()
-    input = strategy.get_input(enqueued_object,
+    inp = strategy.get_input(enqueued_object,
                                storage_directory,
                                task)
-    assert input is not None
+    assert inp is not None
