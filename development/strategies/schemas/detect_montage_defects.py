4d99afa1a5f107250f70a90ae4646d8d98302f12
Tim Fliss <timf@alleninstitute.org>
2018-01-31 14:32:19 -0800

    CONNP-95; missed checkin

---
+++ development/strategies/schemas/detect_montage_defects.py
@@ -0,0 +1,17 @@
+input_dict = {
+    "render":{
+        "host": "http://em-131fs",
+        "port": 8080,
+        "owner": "danielk",
+        "project": "Seams",
+        "client_scripts": "/allen/programs/celltypes/workgroups/em-connectomics/gayathrim/nc-em2/Janelia_Pipeline/render_latest/render-ws-java-client/src/main/scripts"
+    },
+    "prestitched_stack": "2_sections_near_crack_fine_lam_1e3",
+    "poststitched_stack": "2_sections_near_crack_fine_lam_1e3_omitted",
+    "match_collection": "NewPMS_combined_with_montage",
+    "out_html_dir":"/allen/programs/celltypes/workgroups/em-connectomics/gayathrim/scratch",
+    "plot_sections":"True",
+    "minZ": 1028,
+    "maxZ": 1029,
+    "pool_size": 20
+}
