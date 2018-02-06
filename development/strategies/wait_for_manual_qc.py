5e4c4004d3401a167bf7c6c77495784336dcb857
Tim Fliss <timf@alleninstitute.org>
2018-02-06 14:38:54 -0800

    CONNP-95; stub wait state for manual qc

---
+++ development/strategies/wait_for_manual_qc.py
@@ -0,0 +1,10 @@
+from workflow_engine.strategies.wait_strategy \
+    import WaitStrategy
+
+
+class WaitForManualQc(WaitStrategy):
+    def must_wait(self, em_mset):
+        # if em_mset.workflow_state is not None and \
+        #   'TARGET_MONTAGE_QC_PASSED' != em_mset.workflow_state:
+        #    return False
+        return True
