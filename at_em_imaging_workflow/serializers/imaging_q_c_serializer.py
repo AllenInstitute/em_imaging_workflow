639e676e08ac1c18e66fe8a56043408ef2b570ae
Tim Fliss <timf@alleninstitute.org>
2018-07-09 14:08:32 -0700

    CONNP-95; missed checkin

---
+++ at_em_imaging_workflow/serializers/imaging_q_c_serializer.py
@@ -0,0 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import PrimaryKeyRelatedField
+from rest_framework.fields import DateTimeField
+from development.models.e_m_montage_set import EMMontageSet
+
+
+class ImagingQCSerializer(ModelSerializer):
+    acquisition_date = DateTimeField(format='iso-8601')
+    #sample_holder = PrimaryKeyRelatedField(
+    #        many=False,
+    #        read_only=True)
+
+    class Meta:
+        model = EMMontageSet
+        fields = ('id', 'workflow_state', 'sample_holder', 'section')
